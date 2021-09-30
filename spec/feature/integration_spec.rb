3e1c019cf8fd46445f14014b5647099402a04784
Troy Lee <troylee3006@gmail.com>
2021-09-30 13:22:30 -0500

    Book collection assignment tests and CRUD finished

--- spec/feature/integration_spec.rb
+++ spec/feature/integration_spec.rb
@@ -5,6 +5,8 @@ RSpec.describe 'Creating a book', type: :feature do
   scenario 'valid inputs' do
     visit new_book_path
     fill_in 'Title', with: 'harry potter'
+    fill_in 'Author', with: 'harry potter'
+    fill_in 'Price', with: '5'
     click_on 'Create Book'
     visit books_path
     expect(page).to have_content('harry potter')
