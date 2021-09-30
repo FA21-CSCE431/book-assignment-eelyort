194d0b546a55f65d742b688a73bbd24ad32470df
Troy Lee <troylee3006@gmail.com>
2021-09-30 17:27:24 -0500

    delete page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
-  resources :books
+  resources :books do
+    member do
+      get :delete
+    end
+  end
+  resources :pages do
+    member do
+      get :delete
+    end
+  end
   root'books#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
